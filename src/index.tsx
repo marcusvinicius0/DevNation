b416a9b51746fd2b41911998857f6a7c8a3ca26e
Pedro Henrique da Silva <pedro_rike@hotmail.com.br>
2022-08-10 20:18:44 -0300

    feat: changed code service with eslint

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/global.scss';
 
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  )
-
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
